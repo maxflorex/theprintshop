d6c5e7851a78dcf30cc0b573a7db245f645d7057
Max Flores <max@maxflores.dev>
2022-01-31 14:28:10 -0600

    Dynamic Routing

---
+++ pages/api/dataProducts.js
@@ -0,0 +1,82 @@
+const data = [
+    {
+        id: 1,
+        title: 'Canvas',
+        desc: 'The description goes here ;)',
+        features: [
+            {
+                ft1: "Inks",
+                ft2: 'Archival',
+                ft3: 'OBA Free',
+                ft4: 'Made in Cayman'
+            },
+        ],
+        specs: [
+            {
+                texture: 'Medium',
+                finish: 'Matte',
+                weight: '400 GSM',
+            }
+        ],
+    },
+    {
+        id: 2,
+        title: 'Framing',
+        desc: 'The description goes here ;)',
+        features: [
+            {
+                ft1: "Inks",
+                ft2: 'Archival',
+                ft3: 'OBA Free',
+                ft4: 'Made in Cayman'
+            },
+        ],
+        specs: [
+            {
+                texture: 'Medium',
+                finish: 'Matte',
+                weight: '400 GSM',
+            }
+        ],
+    },
+    {
+        id: 3,
+        title: 'Paper',
+        desc: 'The description goes here ;)',
+        features: [
+            {
+                ft1: "Inks",
+                ft2: 'Archival',
+                ft3: 'OBA Free',
+                ft4: 'Made in Cayman'
+            },
+        ],
+        specs: [
+            {
+                texture: 'Medium',
+                finish: 'Matte',
+                weight: '400 GSM',
+            }
+        ],
+    },
+    {
+        id: 4,
+        title: 'Aluminum',
+        desc: 'The description goes here ;)',
+        features: [
+            {
+                ft1: "Inks",
+                ft2: 'Archival',
+                ft3: 'OBA Free',
+                ft4: 'Made in Cayman'
+            },
+        ],
+        specs: [
+            {
+                texture: 'Medium',
+                finish: 'Matte',
+                weight: '400 GSM',
+            }
+        ],
+    },
+]
--- pages/products/canvas.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const canvas = () => {
-  return <div>holi</div>;
-};
-
-export default canvas;
